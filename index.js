751e8a734f44a50b7b11f63ff66d5c8491abfed3
anna <anna.riazantseva123321@gmail.com>
2023-05-15 12:55:16 +0300

    new version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * @param {number} winnerRating 
  * @param {number} opponentRating 
  * @returns {number}
+ * @throws {Error}
  */
 
 function getWinnerRating(winnerRating, opponentRating){
@@ -11,20 +12,29 @@ function getWinnerRating(winnerRating, opponentRating){
     if(winnerRating < 0 ||  opponentRating < 0){
         return "Error";
     }
-    const ratingDifference = opponentRating - winnerRating;
+    if (winnerRating === 0) {
+      return winnerRating + opponentRating;
+    }
+
+// победа над слабым игроком
+    const ratingDifference = winnerRating - opponentRating ;
 
     if (ratingDifference >= 0 && ratingDifference <= 2) {
       return winnerRating + 2;
+
     } if (ratingDifference > 2 && ratingDifference < 20) {
       return winnerRating + 1;
+
     } if (ratingDifference >= 20) {
       return winnerRating;
     } 
+
+  // победа над сильным игроком
         const contribution = (opponentRating - winnerRating + 5) / 3;
     return winnerRating + contribution;
 }
-    const winnerRating = 100;
-    const opponentRating = 50;
+    const winnerRating = 20;
+    const opponentRating = 18;
   
     const result = getWinnerRating(winnerRating, opponentRating);
     console.log(`Rating of the winner: ${result}`);
